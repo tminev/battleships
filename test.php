284f8038a74f60b1d32b204defd157debfe87ea4
Tihomir Minev <tihomir.minev.sci@gmail.com>
2017-07-07 01:01:29 +0300

    final details
    final details

--- test.php
+++ test.php
@@ -10,8 +10,12 @@
 <audio id="shot"><source src="audio/shot.mp3" type="audio/mpeg"></audio>
 <audio id="hit"><source src="audio/hit.mp3" type="audio/mpeg"></audio>
 
+<div id="planets"> 
+<div id="earth">  </div>
+<div id="mars" ></div>
+</div>
 <div id="container">
-	<div id="furst">USER
+	<div id="furst">USER NAME
 					<?php 
 					//my table
 				echo "<table id='table'>";
@@ -25,7 +29,8 @@
 				echo "</table>";
 				?>
 	</div>
-	<div id="second">BOT
+	<div id="second">PC BOT
+
 		<?php
 		$ship_temp=0;
 
@@ -50,14 +55,17 @@
 	?>
 	</div>
 	<div class="result"> <p id="my_score">My Score: 0</p><p id="pc_score">Pc Score: 0</p> </div>
-	<p>Mouse is at coordinates: <span></span>.</p>
+	
 </div>
+<div id="message"> YOU WIN Ma MAN !<div>
+<div id="message2"> YOU LOSE!<div>
 <script type="text/javascript">
+//$("#message").fadeIn(3000);
+
 var temp = 0 , flag =1;
 var shot = 0;
 var miss = 0;
 var pc_score = 0;
-
 
 
 //arrange my ships
@@ -69,76 +77,92 @@ $("#furst td").click(function(){
 	$(this).off('click');
 	temp=temp+1;
 
+
 //array for pc answer
 	function numberArray(a,b){
 			b=[];
-			while(a--)b[a]=a+1;
+			while(a--)b[a]=a;
 			return b;}
 
 	function rand(a,b,c,d){
 			c=a.length;
 			while(c)b=Math.random()*(--c+1)|0,d=a[c],a[c]=a[b],a[b]=d}
-			var a=numberArray(24);
+			var a=numberArray(25);
 			rand(a);
 
-
+console.log(a)
 
 	//my thurn
 	var i =0;
+
 	if (temp==10) { 
-		
+
+		var flag = true;
 		$("#furst td").off('click');
-	   	$("#second td").on('click');
-	   	
-	   
-	    $("#second td").click(function(){ 
+	    $("#second td").on( "click", function() { 
+
+	    if (flag) {
 	    document.getElementById("shot").play(); 
-		$(this).css("background-color","gray");
+		$(this).css("background-color","rgba(158, 158, 158, 0.3");
 		$(this).off('click');
 	   	var shoting = $(this).children("p").html();
-	   
+	   flag = false
 	    if (shoting==1) { 
 	    	shot++;
-	    	$(this).css("background-color","red");
 	    	var img=Math.floor((Math.random() * 4) + 1);
-	    	var imgcrash="<img src=' img/ship" +img+ ".gif ' />";
+	    	var imgcrash="<img src=' img/exp.gif ' />";
 	    	$(this).append(imgcrash);
+	    	var koko = $(this)
+	    	setTimeout(function(){	document.getElementById("hit").play(); 
+	    		koko.children().attr('src', 'img/crash.gif');
+	    	 }, 1000);
+	   		$("#my_score").text("My Score: " + shot);
+	   		 if (shot==10) {
+						 $("#message").fadeIn(3000);
+						 return false;
+			}
+	   	}
 
-	    	setTimeout(function(){	document.getElementById("hit").play();  }, 1000);
-	   		$("#my_score").text("My Score: " + shot);}
 
-	  
 					//pc thurn
 				    setTimeout(function(){	
+				    	flag=true;
 					    var td = document.getElementsByClassName('td_furst')[a[i]];
 					    i++;
-					    td.style.background = "gray";
+					    td.style.background = "rgba(158, 158, 158, 0.3";
 					    document.getElementById("shot").play();
 					    var src = td.querySelector('img').getAttribute('src');
-					  
-
+					  	
 					    if (src) {
-					    	
-					    	td.style.background = "red";
+					   		var c = td.children;
+					   		c[0].src='img/exp.gif'
+					   		setTimeout(function(){	
+					    	c[0].src='img/crash.gif'
+					    	 }, 2000);
 					    	var rnd=Math.floor((Math.random() * 4) + 1);
 					    	setTimeout(function(){	document.getElementById("hit").play();  }, 1000);
 							 
 					    	pc_score++;
 					    	$("#pc_score").text("Pc Score: " + pc_score);
-					    	 console.log(src);
+					    	 
+					    	 if (pc_score==1) {
+							 $("#message2").fadeIn(3000);
+							 return false;
+							}
+					    	 
 
 					    } 
-					     }, 4000);
-
+					}, 4000);
+				}
 		 }); 
-
-
-
 	}
+
+	
+
+
 }); 
 
 // cool
-
 $(document).mousemove( function(e) {
    mouseY = e.pageY;
    mouseX = e.pageX;
@@ -146,11 +170,20 @@ $(document).mousemove( function(e) {
    translateX = 'translateX(' + (mouseX-800)/20 + 'px)';
 
     $('td').css({'transform': translateY});
-     $('tr').css({'transform': translateX});
+    $('tr').css({'transform': translateX});
     
     $('#second').css({'transform': translateX});
-     $('#furst').css({'transform': translateX});
-      $('#container').css({'transform': translateY});
+    $('#furst').css({'transform': translateX});
+    $('#container').css({'transform': translateY});
+
+
+    translatePlanetY = 'translateY(' + (mouseY-200)/100 + 'px)';
+  	translatePlanetX = 'translateX(' + (mouseX-800)/100 + 'px)';
+
+    $('#mars').css({'transform': translatePlanetY})
+    $('#earth').css({'transform': translatePlanetY})
+    $('#planets').css({'transform': translatePlanetX});
+
 });
 
 
